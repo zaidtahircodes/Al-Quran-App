b51c45c51b1e73d8dc51e21a8b033e5b1df9feaa
Zaid Tahir <zaidtahir524@Gmail.com>
2023-10-19 21:10:44 +0500

    fix bug in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 <footer className="bg-white shadow-slate-200 shadow-inner rounded-lg dark:bg-gray-900 m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-                <HashLink smooth to="/#home" className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</HashLink>
+                <HashLink smooth to="/#home" className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Al Quran</HashLink>
     
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
